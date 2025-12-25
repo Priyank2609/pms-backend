8028964f281d94f41c8ce2182d76d1417ce9611a
Priyank <priyankprajapati1209@gmail.com>
2025-12-25 11:27:03 +0530

    path

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.use(cookieParser())
 
 
 const corUrl = {
-  origin: 'https://frontend-di6mkpwv9-priyank2609s-projects.vercel.app'
+  origin: 'https://frontend-di6mkpwv9-priyank2609s-projects.vercel.app',
+  credentials: true,
 }
 app.use(cors(corUrl))
 
