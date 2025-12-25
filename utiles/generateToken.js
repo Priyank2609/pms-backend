ace8f65458f7251eb2d6129eb2fb9239a3bf1df5
Priyank <priyankprajapati1209@gmail.com>
2025-12-25 11:57:55 +0530

    path

--- utiles/generateToken.js
+++ utiles/generateToken.js
@@ -8,6 +8,8 @@ module.exports.generateToken = async (user, res) => {
 
   res.cookie("JWT_Token", token, {
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
   })
   return token
